2b8e5880229e036e8d06f322445131b9c9af6d88
franck-artaud <franck-artaud@atelier601.com>
2019-03-04 16:41:48 +0100

    vide34

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt')
+
+
 const PORT = 5000;
+let frenchMovies = [];
 
 app.use('/public', express.static('public'))
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+const secret = 'qsdjS12ozehdoIJ123DJOZJLDSCqsdeffdg123ER56SDFZedhWXojqshduzaohduihqsDAqsdq';
+
+
+app.use(expressJwt({secret: secret}).unless({path: ['/', '/movies', '/movie-search', '/login']}));
+
+
 
 app.set('views', './views');
 app.set('view engine','ejs');
@@ -13,7 +29,7 @@ app.get('/movies', (req, res) => {
 
   const title= 'Les films français des 30 dernières années';
 
-  const frenchMovies = [
+ frenchMovies = [
       { title: 'Le fabuleux destin d\'Amélie Poulain', year:'2001'},
       { title: 'Buffet froid', year:'1979'},
       { title: 'Le diner de con ', year:'1978'},
@@ -23,6 +39,33 @@ app.get('/movies', (req, res) => {
   //res.send('Bientôt des films ici');
   res.render('movies', {movies:frenchMovies, title:title});
 });
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Travailler avec Multer
+//     app.post('/movies', urlencodedParser,(req, res) => {
+//     console.log('Le titre : ', req.body.movietitle);
+//     console.log('Année : ', req.body.movieyear);
+//     const newMovie = {title : req.body.movietitle, year: req.body.movieyear };
+//     //frenchMovies.push(newsMovie);
+//     frenchMovies = [...frenchMovies, newMovie];
+//     console.log(frenchMovies);
+//     res.sendStatus(201);
+// })
+
+app.post('/movies', upload.fields([]), (req,res)  =>{
+    if(!req.body) {
+        return res.sendStatus(500);
+    } else {
+        const formdata = req.body;
+        console.log('forData: ', formdata );
+        const newMovie = {title : req.body.movietitle, year: req.body.movieyear };
+        frenchMovies = [...frenchMovies, newMovie];
+        res.sendStatus(201);
+    }
+})
+
+
+
 
 //app.get('/movie-detail', (req, res) => {
     //res.send('Bientôt des films ici');
@@ -47,6 +90,44 @@ app.get('/', (req, res) => {
     res.render('index');
 
 });
+
+app.get('/movie-search', (req,res) =>{
+    res.render('movie-search');
+})
+
+app.get('/login', (req,res) => {
+    res.render('login', {title : 'Espace membre'});
+});
+
+const fakeUser = { email: 'testuser@testmail.fr', password: 'qsd' };
+
+
+app.post('/login' , urlencodedParser, (req,res) => {
+    console.log('login post', req.body);
+    if(!req.body) {
+        res.sendStatus(500);
+    } else {
+        if(fakeUser.email === req.body.email && fakeUser.password === req.body.password) {
+            const myToken = jwt.sign({iss :'http://expressmovie.fr', user:'Sam',role:'moderator'}, secret);
+            res.json(myToken);
+            // res.json ({
+            //     email:'testuser@testmail.fr',
+            //     favoriteMovie:'Il était une fois dans l\'ouest',
+            //     favoriteMovieTheater:'Ciné TMB, 1 rue Saint-Hellier, 35040 Rennes',
+            //     lastLoginDate:new Date()
+            // });
+        } else {
+            res.sendStatus(401);
+        }
+    }
+});
+
+
+app.get('/member-only', (req,res,) => {
+    console.log('req.user', req.user);
+    res.send(req.user);
+});
+
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
